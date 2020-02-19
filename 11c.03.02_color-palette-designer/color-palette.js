f1a028d324d0bb0ee9c591e44d398b5bff3bd116
megane <megane.londono@hotmail.com>
2020-02-19 10:19:55 +0100

    added files

---
+++ 11c.03.02_color-palette-designer/color-palette.js
@@ -0,0 +1,40 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+let HTML = {};
+
+function init() {
+
+    baseColor();
+}
+
+// <input type="color"> for selecting base color (in the middle)
+function baseColor() {
+
+}
+
+// one function per color harmony
+function analogous() {
+
+}
+
+function monochromatic() {
+
+}
+
+function triad() {
+
+}
+
+function complementary() {
+
+}
+
+function compound() {
+
+}
+
+function shades() {
+
+}
